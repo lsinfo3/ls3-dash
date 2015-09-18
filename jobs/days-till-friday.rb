c18aea5c049b98b011bdb907d6992ba6230d23b0
Florian Wamser <florian.wamser@informatik.uni-wuerzburg.de>
2015-09-18 18:56:03 +0200

    Immense, outstanding improvements on the till-friday widget

--- jobs/days-till-friday.rb
+++ jobs/days-till-friday.rb
@@ -1,15 +1,20 @@
-
-SCHEDULER.every '2s' do
+SCHEDULER.every '1200s', :first_in => 2 do
 
   date = Time.new
-  day = date.strftime("%w")
+  day = date.strftime "%w"
 
-  to_friday = day.to_i - 5
-  if day.to_i == 5
-	  send_event('days-till-friday', { title: "Today", text: "is Friday!" })
+  # (Sunday is 0, 0..6)
+  if day.to_i == 4
+	  data = { title: "Tomorrow", text: "is Friday!" }
+  elsif day.to_i == 5
+	  data = { title: "Today", text: "is Friday!" }
+  elsif (day.to_i == 6) or (day.to_i == 0)
+	  data = { title: "Today", text: "is the weekend!" }
   else
-	  send_event('days-till-friday', { title: to_friday.to_s + " days" })
+  	to_friday = 5 - day.to_i
+	data = { title:  to_friday.to_s + " days", text: "till Friday." }
   end
   
-  send_event('days-till-friday', { moreinfo: "Current date: " + date.strftime("%A, %d.%m.%Y") })
+  data.merge!(moreinfo: "Current date: " + date.strftime("%A, %d.%m.%Y"))
+  send_event('days-till-friday', data)
 end
