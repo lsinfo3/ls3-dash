e4195a3c3e269daed869d93fc77dc437a18f5a8c
Christian Schwartz <christian.schwartz@gmail.com>
2015-09-18 09:12:06 +0200

    first draft on coffee

--- jobs/coffee.rb
+++ jobs/coffee.rb
@@ -1,8 +1,11 @@
 require 'mechanize'
 require 'benchmark'
+
+cooking_threshold = 500
+
 points = []
 
-(1..10).each do |i|
+(1..120).each do |i|
   points << { x: i, y: 0 }
 end
 last_x = points.last[:x]
@@ -13,7 +16,10 @@ raritan_password = ENV['RARITAN_PASSWORD']
 
 m = Mechanize.new { |a| a.ssl_version, a.verify_mode = 'SSLv3', OpenSSL::SSL::VERIFY_NONE}
 
-SCHEDULER.every '5s', first_in: 0 do
+last_coffee = DateTime.now
+is_cooking = false
+
+SCHEDULER.every '15s', first_in: 0 do
   m.get(url) do |page|
     form = page.form_with(:action => '/auth.asp') do |f|
       break if f.nil?
@@ -26,8 +32,15 @@ SCHEDULER.every '5s', first_in: 0 do
     watt = page.iframes.last.content.search('.dtbl tr:nth-child(2) td:nth-child(6)').text.strip.split(/ /).first.to_f
 
     last_x += 1
-    points << { x: last_x, y: watt }
+    points.shift
+    points.push({ x: last_x, y: watt })
 
-    send_event('coffee', points: points)
+    previous_is_cooking = is_cooking
+    is_cooking = watt > cooking_threshold ? 'filling' : 'unknown'
+    last_coffee =  DateTime.now if previous_is_cooking && !is_cooking
+
+    data = { points: points, coffee_status: is_cooking, last_coffee: last_coffee.to_s }
+    p data
+    send_event('coffee', data)
   end
 end
