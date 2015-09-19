63cc864879d115b15f28e53f032dd9615c33ac29
Florian Wamser <florian.wamser@informatik.uni-wuerzburg.de>
2015-09-19 02:23:13 +0200

    beautify code

--- jobs/days-till-friday.rb
+++ jobs/days-till-friday.rb
@@ -1,9 +1,8 @@
 SCHEDULER.every '1200s', :first_in => 2 do
 
   date = Time.new
-  day = date.strftime "%w"
-
-  # (Sunday is 0, 0..6)
+  day = date.strftime "%w"  # (Sunday is 0, 0..6)
+  
   if day.to_i == 4
 	  data = { title: "Tomorrow", text: "is Friday!" }
   elsif day.to_i == 5
@@ -12,9 +11,9 @@ SCHEDULER.every '1200s', :first_in => 2 do
 	  data = { title: "Today", text: "is the weekend!" }
   else
   	to_friday = 5 - day.to_i
-	data = { title:  to_friday.to_s + " days", text: "till Friday." }
+	data = { title:  "#{to_friday} days", text: "till Friday." }
   end
   
-  data.merge!(moreinfo: "Current date: " + date.strftime("%A, %d.%m.%Y"))
-  send_event('days-till-friday', data)
+  data.merge! moreinfo: "Current date: %s" % date.strftime("%A, %d.%m.%Y")
+  send_event 'days-till-friday', data
 end
