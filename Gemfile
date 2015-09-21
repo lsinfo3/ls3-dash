b58aeff6e86e096605424449730cc49a453225ab
Christian Schwartz <christian.schwartz@gmail.com>
2015-09-21 11:55:50 +0200

    dees

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'dashing'
+gem 'dashing-contrib', '~> 0.2.1'
 
 ## Remove this if you don't need a twitter widget.
 gem 'twitter', '>= 5.9.0'
--- jobs/dees.rb
+++ jobs/dees.rb
@@ -1,27 +1,20 @@
 require 'nokogiri'
 require 'open-uri'
 require 'yomu'
-
-# monkeypatch a deeper to_json in Struct
-class Struct
-  def to_h
-    Hash[ self.members.zip(self.to_a) ]
-  end
-end
-
-Dish = Struct.new 'Dish', :name, :price
-Day = Struct.new 'Day', :dishes
-Menu = Struct.new 'Menu', :days
+require 'date'
 
 SCHEDULER.every '10m', first_in: 0 do
   site = Nokogiri::HTML(open('http://www.metzgerei-dees.de/'))
   menu_plan_url = site.css('#menu-main-nav li:last a').last.attr('href')
   menu_plan = Yomu.new(URI.encode(menu_plan_url)).text
 
+  today = Date.today
+  tomorrow = today + 1
+
   current_dish = nil
   current_day = nil
   days = %w(monday thuesday wednesday thursday friday).each
-  menu = Menu.new({})
+  menu = []
 
   current_line = 0
 
@@ -30,24 +23,29 @@ SCHEDULER.every '10m', first_in: 0 do
 
     line.strip!
     if current_line == 0
-      current_day = Day.new []
+      current_day = { items: [] }
     elsif current_line == 2 || current_line == 7
-      current_dish = Dish.new line
+      current_dish = { label: line }
     elsif current_line == 3 || current_line == 8
-      current_dish.name += " #{ line }"
+      current_dish[:label] += " #{ line }"
     elsif current_line == 5 || current_line == 10
-      current_dish.price = line
+      current_dish[:count] = line
     elsif current_line == 6 || current_line == 11
-      current_day.dishes << current_dish
+      current_day[:items] << current_dish
     end
 
     if current_line == 11
-      menu.days[days.next] = current_day
+      current_day[:label] = days.next.titlecase
+      menu << current_day
       current_line = -1
     end
 
     current_line += 1
   end
 
-  send_event('dees', menu: menu)
+  p relevant_menus = menu.each_with_index.select { |entry, index|
+    (index + 1 == today.wday) || (index + 1 == tomorrow.wday)
+  }.map { |entry, index| entry }
+
+  send_event('dees', data: relevant_menus)
 end
