1a2beef6918e69cdc4d7cb1b83ec13df8d5b0d2e
Florian Wamser <florian.wamser@informatik.uni-wuerzburg.de>
2015-10-23 11:49:18 +0200

    Adding Mensa Hubland

---
+++ jobs/hublandmensa.rb
@@ -0,0 +1,30 @@
+require 'nokogiri'
+require 'open-uri'
+require 'date'
+
+url = 'https://www.studentenwerk-wuerzburg.de/wuerzburg/essen-trinken/speiseplaene.html?tx_thmensamenu_pi2[mensen]=7&tx_thmensamenu_pi2[action]=show&tx_thmensamenu_pi2[controller]=Speiseplan&cHash=efe40abc8afe9bcac3abf914dff9d943'
+
+today = Date.today
+tomorrow = today + 1
+
+def format_date(date)
+  date.strftime '%d.%m.'
+end
+
+def menu_for(date, site)
+  site.css("div[data-day~='#{ format_date date }'] .menu").map do |e|
+    {
+      label: e.css('.left .title').text.strip,
+      count: e.css('.price').attr('data-bed').value
+    }
+  end
+end
+
+SCHEDULER.every '30m', first_in: 0 do
+  site = Nokogiri::HTML(open(url))
+
+  send_event 'mensahubland', data: [
+    {label: 'Today', items: menu_for(today, site)},
+    {label: 'Tomorrow', items: menu_for(tomorrow, site)}
+  ]
+end
