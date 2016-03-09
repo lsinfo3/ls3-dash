6548864976aba04ade1ba9feac10dec09409e798
Florian Wamser <florian.wamser@informatik.uni-wuerzburg.de>
2016-03-09 10:28:07 +0100

    Fix bug uptime

--- jobs/uptime.rb
+++ jobs/uptime.rb
@@ -5,7 +5,10 @@ SCHEDULER.every '10s', first_in: 0 do
 
     # get uptime
     uptime = IO.read('/proc/uptime').split[0].to_i
-    up_str = Time.at(uptime).utc.strftime("%-m months, %-d days, %-H hours")
+    days = uptime / (60 * 60 * 24)
+    hours = (uptime / (60 * 60)) % 24
+    minutes = (uptime / 60) % 60
+    up_str = format("%d days, %d hours, %d mins", days, hours, minutes)
 
     # post results
     results = {
