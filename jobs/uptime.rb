27b18fc881a90c02729672e2b8c1d237eaa6fcbe
Florian Wamser <florian.wamser@informatik.uni-wuerzburg.de>
2016-03-09 09:49:43 +0100

    Uptime

---
+++ jobs/uptime.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+SCHEDULER.every '10s', first_in: 0 do
+
+    # get uptime
+    uptime = Time.now - IO.read('/proc/uptime').split[0].to_i
+    up_str = Time.at(uptime).utc.strftime("%-m months, %-d days, %-H hours")
+
+    # post results
+    results = {
+      title: uptime,
+      text: 'till last AcCident.'
+    }
+
+    send_event "uptime", results
+  end
+end
