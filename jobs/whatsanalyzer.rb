47eba4d324dbb318906c4ecf4c0f1b8672a5e3bd
Florian Wamser <florian.wamser@informatik.uni-wuerzburg.de>
2016-04-12 14:24:11 +0200

    Whatsanalyzer update

--- jobs/whatsanalyzer.rb
+++ jobs/whatsanalyzer.rb
@@ -2,22 +2,27 @@ require 'mechanize'
 require 'benchmark'
 require 'rest_client'
 require 'json'
+require 'date'
 
 # settings
-url = 'http://whatsanalyzer.informatik.uni-wuerzburg.de/admin/dashboard.jsp'
+url = 'https://whatsanalyzer.informatik.uni-wuerzburg.de/admin/dashboard.jsp'
+old_value = [0, 0, 0, 0, 0, 0, 0]
 
 # query page
-m = Mechanize.new { |a| a.ssl_version, a.verify_mode = 'SSLv3', OpenSSL::SSL::VERIFY_NONE }
+m = Mechanize.new { OpenSSL::SSL::VERIFY_NONE }
 m.add_auth(url, 'michi', '')
-SCHEDULER.every '1m', first_in: 0 do
+SCHEDULER.every '30m', first_in: 0 do
   m.get(url) do |page|
     # get current chats
     # puts page.at('head').text
-    num_chats = page.at('head').text.match(/numOfChatsToday = ([0-9]+);/i).captures
+    #num_chats = page.at('head').text.match(/numOfChatsToday = ([0-9]+);/i).captures
+    num_chats = page.at('body').text.match(/Insgesamt gesammelte Chats: ([0-9]+)/i).captures
     #puts num_chats
+    old_value[DateTime.now.wday] = num_chats
 
     # send updates to board
-    send_event('whatsanalyzer', { current: num_chats, last: 0 })
+    old = old_value[(DateTime.now.wday - 1) % 7]
+    send_event('whatsanalyzer', { current: num_chats, last: old })
   end
 end
 
