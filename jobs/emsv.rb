ae1f0b5932397d1f7531b4336e40d0804c4820bc
Florian Wamser <florian.wamser@informatik.uni-wuerzburg.de>
2016-06-15 22:06:39 +0200

    Upcoming and past machtes for emsv dashboard

--- jobs/emsv.rb
+++ jobs/emsv.rb
@@ -4,7 +4,7 @@ require 'mysql2'
 db_url = ENV['EMSV_DB_URL']
 user = ENV['EMSV_DB_USER']
 pass = ENV['EMSV_DB_PASS']
-
+p db_url
 
 SCHEDULER.every '5m', first_in: 0 do
 
@@ -42,11 +42,37 @@ SCHEDULER.every '5m', first_in: 0 do
     }
   end
 
+  # Mysql query
+  sql = "SELECT * FROM games WHERE CAST(CONCAT(date,' ',time) AS DATETIME) < (NOW()) AND date >= CURDATE()-2 ORDER BY date, number;"
+  results = db.query(sql)
+  matches_past = results.map do |row|
+    label = row['date'].to_s + ' ' + row['time'].to_s[11..15] + ' > ' + row['team1'] + ' - ' + row['team2']
+    result = row['goals1'].to_s +  ':' + row['goals2'].to_s
+    row = {
+      :label => label,
+      :count => result
+    }
+  end
+
+  # Mysql query
+  sql = "SELECT * FROM games WHERE CAST(CONCAT(date,' ',time) AS DATETIME) >= (NOW()) AND date <= CURDATE()+2 ORDER BY date, number;"
+  results = db.query(sql)
+  matches_upcoming = results.map do |row|
+    label = row['date'].to_s + ' ' + row['time'].to_s[11..15] + ' > ' + row['team1'] + ' - ' + row['team2']
+    row = {
+      :label => label,
+      :count => ''
+    }
+  end
+
+
+
 
 
 
   send_event 'ranking-top10', data: [{ label: 'Top 10', items: top10 }]
   send_event 'ranking-exakt', data: [{ label: 'Exact Results', items: exakt }]
   send_event 'ranking-trend', data: [{ label: 'Trend', items: trend }]
+  send_event 'games', data: [{ label: 'Past Matches', items: matches_past }, { label: 'Upcoming Matches', items: matches_upcoming }]
   puts 'new run'
 end
